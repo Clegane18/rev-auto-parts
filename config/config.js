0cc061fd2fefac1c39f211d5570590eeb47f9fa5
Clegane18 <126550063+Clegane18@users.noreply.github.com>
2024-10-09 13:55:57 +0800

    fix mismatch in production and development

--- config/config.js
+++ config/config.js
@@ -7,6 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    logging: console.log,
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -14,6 +15,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    logging: false,
   },
   production: {
     url: process.env.DATABASE_URL,
@@ -27,3 +29,5 @@ module.exports = {
     },
   },
 };
+
+// NEW
--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,29 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const {
-  dbPassword,
-  dbUserName,
-  dbName,
-  dbHost,
-} = require("../utils/passwordUtils");
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
-  host: dbHost,
-  dialect: "postgres",
-  timezone: "+08:00",
-  logging: false, // Set to true if needed for debugging purposes
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  const { DB_PASSWORD, DB_USERNAME, DB_NAME, DB_HOST } = process.env;
+
+  sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: "postgres",
+    timezone: "+08:00",
+    logging: console.log,
+  });
+}
 
 module.exports = sequelize;
+// New
