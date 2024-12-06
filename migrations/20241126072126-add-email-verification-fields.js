273876fc0b8c05fdb03e49b887313ae47c2ee08c
Clegane18 <126550063+Clegane18@users.noreply.github.com>
2024-12-06 22:10:12 +0800

    fix the migration issue

--- migrations/20241126072126-add-email-verification-fields.js
+++ migrations/20241126072126-add-email-verification-fields.js
@@ -2,23 +2,13 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Customers", "emailVerified", {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    });
-
-    await queryInterface.addColumn("Customers", "emailVerificationToken", {
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
-
-    await queryInterface.addColumn("Customers", "verificationTokenExpires", {
-      type: Sequelize.DATE,
-      allowNull: true,
-    });
+    const tableDescription = await queryInterface.describeTable("Customers");
+    if (!tableDescription.emailVerified) {
+      await queryInterface.addColumn("Customers", "emailVerified", {
+        type: Sequelize.BOOLEAN,
+      });
+    }
   },
-
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn("Customers", "emailVerified");
     await queryInterface.removeColumn("Customers", "emailVerificationToken");
